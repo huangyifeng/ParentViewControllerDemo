a20a3d4480d7ce5047bce5c336214a27a966428c
huangyifeng <luckyJerry85@gmail.com>
2012-11-23 15:08:43 +0800

    Initial Commit

---
+++ ParentViewControllerDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ParentViewControllerDemo
+//
+//  Created by HuangYiFeng on 11/23/12.
+//  Copyright (c) 2012 cybozu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ParentViewControllerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParentViewControllerDemo
+//
+//  Created by HuangYiFeng on 11/23/12.
+//  Copyright (c) 2012 cybozu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
